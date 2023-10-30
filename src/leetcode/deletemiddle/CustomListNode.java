9a85b2d552dcb73d9250a37ceec27ce3ab73dfca
EminYagiz <eminyagiz42@gmail.com>
2023-10-30 20:44:52 +0300

    DeleteMiddleNode leetcode question is solved

---
+++ src/leetcode/deletemiddle/CustomListNode.java
@@ -0,0 +1,18 @@
+package leetcode.deletemiddle;
+
+public class CustomListNode {
+
+    int val;
+    CustomListNode next;
+
+    CustomListNode() { }
+
+    CustomListNode(int val) {
+        this.val = val;
+    }
+
+    CustomListNode(int val, CustomListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
