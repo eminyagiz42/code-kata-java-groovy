c083f68128a59ed56ef51f139d5c7b642a7aab2d
EminYagiz <eminyagiz42@gmail.com>
2023-10-30 20:34:51 +0300

    Added libraries: Groovy, Apache commons lang3

---
+++ src/commonlang/CommonsLangPractice.java
@@ -0,0 +1,14 @@
+package commonlang;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+public class CommonsLangPractice {
+
+    public static void main(String[] args) {
+
+        StringUtils.isMixedCase("abC");
+        StringUtils.isNotEmpty("test");
+        String str = StringUtils.EMPTY;
+    }
+}
