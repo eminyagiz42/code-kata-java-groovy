d81a39b02d9655fed9af98ea65b3e91681aa34dc
EminYagiz <eminyagiz42@gmail.com>
2023-11-24 11:21:00 +0300

    Solved leetcode NumTilings question

--- src/main/java/com/codekata/leetcode75/third25/NumTilings.java
+++ src/main/java/com/codekata/leetcode75/third25/NumTilings.java
@@ -1,6 +1,7 @@
 package com.codekata.leetcode75.third25;
 
 public class NumTilings {
+
     public static void main(String[] args) {
         //https://leetcode.com/problems/domino-and-tromino-tiling
         System.out.println(numTilings(5));
@@ -8,14 +9,14 @@ public class NumTilings {
 
     public static int numTilings(int n) {
         final int kMod = 1_000_000_007;
-        long[] dp = new long[1001];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
+        long[] results = new long[1001];
+        results[1] = 1;
+        results[2] = 2;
+        results[3] = 5;
 
-        for (int i = 4; i <= n; ++i)
-            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % kMod;
-
-        return (int) dp[n];
+        for (int i = 4; i <= n; ++i) {
+            results[i] = (2 * results[i - 1] + results[i - 3]) % kMod;
+        }
+        return (int) results[n];
     }
 }
