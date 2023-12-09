7330e3e6506be4d11397e11ed6a634acf76b2cd4
EminYagiz <eminyagiz42@gmail.com>
2023-12-09 23:58:53 +0300

    Solved hackerrank problem ImplementedInterface

--- src/main/java/com/codekata/hackerrank/mycalculator/ImplementedInterface.java
+++ src/main/java/com/codekata/hackerrank/mycalculator/ImplementedInterface.java
@@ -15,9 +15,6 @@ class ImplementedInterface {
         sc.close();
     }
 
-    /*
-     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
-     */
     static void ImplementedInterfaceNames(Object o) {
         Class[] theInterfaces = o.getClass().getInterfaces();
         for (int i = 0; i < theInterfaces.length; i++) {
