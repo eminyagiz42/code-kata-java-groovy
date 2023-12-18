1258b43780f35068e76c202fda95fff37e8c3c0c
EminYagiz <eminyagiz42@gmail.com>
2023-12-18 15:39:29 +0300

    Solved hackerrank problems SingletonProblem, VisitorPatternProblem

---
+++ src/main/java/com/codekata/hackerrank/SingletonProblem.java
@@ -0,0 +1,31 @@
+package com.codekata.hackerrank;
+
+import java.util.Scanner;
+
+public class SingletonProblem {
+
+    //https://www.hackerrank.com/challenges/java-singleton/problem
+
+    public static final Scanner in = new Scanner(System.in);
+    public String str = "Hello I am a singleton! Let me say %s to you";
+
+    private static SingletonProblem instance;
+
+    private SingletonProblem() {
+    }
+
+    public static SingletonProblem getSingleInstance(){
+        if(instance == null){
+            instance = new SingletonProblem();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner s = new Scanner(System.in)) {
+            final String line = s.nextLine();
+            final SingletonProblem singleInstance = getSingleInstance();
+            System.out.printf((singleInstance.str) + "%n", line);
+        }
+    }
+}
