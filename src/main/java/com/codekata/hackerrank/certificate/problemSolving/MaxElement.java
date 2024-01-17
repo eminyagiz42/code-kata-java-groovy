971b07afb8765a844a000593c4961df5ef19f173
EminYagiz <eminyagiz42@gmail.com>
2024-01-17 21:15:38 +0300

    Hackerrank Intermediate Problem Solving Certificate Attempted

---
+++ src/main/java/com/codekata/hackerrank/certificate/problemSolving/MaxElement.java
@@ -0,0 +1,68 @@
+package com.codekata.hackerrank.certificate.problemSolving;
+
+
+import java.io.*;
+
+
+public class MaxElement {
+
+    public static void main(String[] args) throws IOException {
+        /*
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        int maxSum = Integer.parseInt(bufferedReader.readLine().trim());
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+        int result = Result.maxElement(n, maxSum, k);
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+        bufferedReader.close();
+        bufferedWriter.close();
+        */
+        //int n = 3, maxSum = 7, k = 1; //expected output: 3  --> 1,2,1
+        int n = 4, maxSum = 6, k = 2; //expected output: 2 --> 1,1,2,1
+        int result = maxElement(n, maxSum, k);
+        System.out.println(result);
+    }
+
+    /*
+     * Complete the 'maxElement' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER maxSum
+     *  3. INTEGER k
+     */
+    public static int maxElement(int n, int maxSum, int k) { // It is not completed since limited time in certification
+        int[] arr = new int[n];
+
+        int index = k;
+        for (int i = 0; i < n; i++) {
+            arr[i] = index;
+            index++;
+        }
+
+        for (int j = arr.length - 1; j >= 0; j--) {
+            if (isPossible(maxSum, arr)) {
+                int highest = 0;
+                for (int element : arr) {
+                    highest = Math.max(highest, element);
+                }
+                return highest;
+            }
+            arr[j] -= k;
+        }
+
+        return -1;
+    }
+
+    private static boolean isPossible(int maxSum, int[] arr) {
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+        }
+
+        return sum < maxSum;
+    }
+}
---
+++ src/main/java/com/codekata/hackerrank/certificate/problemSolving/RenameFile.java
@@ -0,0 +1,87 @@
+package com.codekata.hackerrank.certificate.problemSolving;
+
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class RenameFile {
+
+    public static void main(String[] args) throws IOException {
+        /*
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        String newName = bufferedReader.readLine();
+        String oldName = bufferedReader.readLine();
+        int result = Result.renameFile(newName, oldName);
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+        bufferedReader.close();
+        bufferedWriter.close();
+         */
+
+        String newName = "aba";
+        String oldName = "ababa";
+        int result = Result.renameFile(newName, oldName);
+        System.out.println(result);
+    }
+}
+
+
+class Result {
+
+    private final static int M = 1000000007;
+
+    /*
+     * Complete the 'renameFile' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING newName
+     *  2. STRING oldName
+     */
+    public static int renameFile(String newName, String oldName) {
+        int n = oldName.length();
+        int[] arrayDpFirst = new int[n + 1];
+        Arrays.fill(arrayDpFirst, 1);
+        for (int i = 1; i <= newName.length(); i++) {
+            int[] arrayDpSecond = new int[n + 1];
+            for (int j = i; j <= n; j++) {
+                arrayDpSecond[j] = arrayDpSecond[j - 1];
+                if (newName.charAt(i - 1) == oldName.charAt(j - 1))
+                    arrayDpSecond[j] = ((arrayDpSecond[j] % M) + (arrayDpFirst[j - 1] % M)) % M;
+            }
+            arrayDpFirst = arrayDpSecond;
+        }
+        return arrayDpFirst[n] % M;
+    }
+
+    public static int renameFile2(String newName, String oldName) { // Not completed
+
+        final int size = oldName.length() - newName.length();
+
+        StringBuilder builder = new StringBuilder();
+        int index = 0;
+        for (int i = 0; i < oldName.length(); i++) {
+            for (int j = 0; j < newName.length(); j++) {
+                if (j == i || j == i + 1) {
+
+                } else {
+                    builder.append(oldName.charAt(j));
+                }
+            }
+            builder.append(" ");
+        }
+        System.out.println(builder);
+
+        /*
+        for (int i = 0; i <= oldName.length() - newName.length() ; i++) {
+            String subsequence = oldName.substring(i, i + newName.length() );
+            System.out.println(subsequence);
+        }
+        */
+
+        return -1;
+    }
+
+
+}
