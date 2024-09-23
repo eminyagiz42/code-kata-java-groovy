e742300400e00ce746ce7da0580a6f0cab234979
Emin <eminyagiz42@gmail.com>
2024-09-23 13:42:42 +0300

    PaginationDemo is investigated

---
+++ src/main/java/com/codekata/practice/pagination/PaginationDemo.java
@@ -0,0 +1,42 @@
+package com.codekata.practice.pagination;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PaginationDemo {
+
+    public static void main(String[] args) {
+
+        String input = "3";
+        List<String> yourlist = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+
+        int partition = StringUtils.isNumeric(input) ? Integer.parseInt(input) : 1;
+
+        List<List<String>> subLists = getSubLists(yourlist, partition);
+
+        for (List<String> list : subLists) {
+            System.out.println(list);
+        }
+    }
+
+    private static List<List<String>> getSubLists(List<String> urllist, int partition) {
+        List<List<String>> partitions = new ArrayList<>();
+        for (int i = 0; i < urllist.size(); i += partition) {
+            partitions.add(urllist.subList(i, Math.min(i + partition, urllist.size())));
+        }
+        return partitions;
+    }
+}
+
+/*
+Output:
+
+[1, 2, 3]
+[4, 5, 6]
+[7, 8, 9]
+[10]
+
+ */
