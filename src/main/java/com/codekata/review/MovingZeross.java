0de9255704822c3be8361566401e71a934730a31
Emin <eminyagiz42@gmail.com>
2025-07-02 11:11:04 +1200

    Second solution for move zeros questions, starting from the end and swapping number pair

---
+++ src/main/java/com/codekata/review/MovingZeross.java
@@ -0,0 +1,32 @@
+package com.codekata.review;
+
+public class MovingZeross {
+
+    public static void main(String[] args) {
+
+        //https://leetcode.com/problems/move-zeroes
+
+        int[] nums = {0, 1, 0, 3, 12};
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        moveZerosBySwap(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    private static void moveZerosBySwap(int[] nums) {
+        int temp;
+        for (int i = nums.length -1; i > 0; i--) {
+            if (nums[i] == 0) {
+                temp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = temp;
+            }
+        }
+    }
+
+
+}
