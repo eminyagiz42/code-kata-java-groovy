8ebf685dc7cfdb2fc18b039fa520abc5784efe62
Emin <eminyagiz42@gmail.com>
2026-04-15 12:07:17 +1200

    test github actions

--- src/test/java/com/codekata/CodeKataTest.java
+++ src/test/java/com/codekata/CodeKataTest.java
@@ -19,11 +19,34 @@ public class CodeKataTest {
 
     @Test
     public void isGreaterTest() {
-
         LargerNumber largerNumber = new LargerNumber();
         int num1 = 4, num2 = 3;
         assertTrue(num1 + " is greater than " + num2, largerNumber.isGreater(num1, num2));
         System.out.println(num1 + " is greater than " + num2 + " = " + largerNumber.isGreater(num1, num2));
+    }
+
+    @Test
+    public void testPositiveNumbers() {
+        LargerNumber largerNumber = new LargerNumber();
+        assertTrue("10 should be greater than 5", largerNumber.isGreater(10, 5));
+    }
+
+    @Test
+    public void testNegativeNumbers() {
+        LargerNumber largerNumber = new LargerNumber();
+        assertTrue("-1 should be greater than -5", largerNumber.isGreater(-1, -5));
+    }
+
+    @Test
+    public void testEqualNumbers() {
+        LargerNumber largerNumber = new LargerNumber();
+        Assert.assertFalse("5 should not be greater than 5", largerNumber.isGreater(5, 5));
+    }
+
+    @Test
+    public void testLargeNumbers() {
+        LargerNumber largerNumber = new LargerNumber();
+        assertTrue("1000000 should be greater than 999999", largerNumber.isGreater(1000000, 999999));
     }
 
     @After
